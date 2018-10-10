e6867c984ee71a443ca3c2955285a73171458e00
Josiah Maina <josiah5115@gmail.com>
2018-10-10 12:28:54 -0600

    Send a query to get data/POST request

--- client/src/Links.js
+++ client/src/Links.js
@@ -1,23 +1,37 @@
 
 import React, { Component } from 'react';
-import gql from 'graphql-tag'
+import gql from 'graphql-tag';
+import axios from 'axios';
+
 
 export default class Links extends Component {
-   render() {
-     return() {
-       <div>
-         Links will go here
-       </div>
+  componentDidMount() {
+        axios({
+            method: "POST",
+            url: "http://localhost:4000/",
+            data:{
+               query: LINKS_QUERY
+            }
+        }).then((response) => {
+            console.log(response.data)
+        })
+  }
+
+    render() {
+      return(
+        <div>
+          Links will go here
+        </div>
+      )
+    }
+ }
+
+ const LINKS_QUERY = gql `
+   query linksQuery {
+     links {
+       id
+       url
+       description
      }
    }
-}
-
-const LINKS_QUERY = gql `
-  query linksQuery {
-    links {
-      id
-      url
-      description
-    }
-  }
 `
