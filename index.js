277ec616531415fcad7d129574254a74eccbc841
Josiah Maina <josiah5115@gmail.com>
2018-10-10 11:55:20 -0600

    getting info/data back

--- index.js
+++ index.js
@@ -17,12 +17,29 @@ type Link {
 }
 `
 
+// This is dummy text, in a rel app we would use real data;
+let articleLinks = [{
+ id: 'link-0',
+ url: 'www.howtographql.com',
+ description: 'A resources to learn graphql. Check out the advanced sections for some more in-depth tutorials.'
+}, {
+ id: 'link-1',
+ url: 'news.ycombinator.com',
+ description: 'Hacker news is like reddit that doesn\'t suck.  Focused on tech.  Great place to improvey our chameleon skills.'
+}, {
+ id: 'link-2',
+ url: 'https://www.graphqlhub.com/',
+ description: 'Some practice APIs to play around with queries'
+}]
+let idCount = articleLinks.length
+
 // This is the object that needs to match the stracture of our typeDefination Queries and Muatation
 // All values should be functions and what they return is lie doing res.send...and
 const resolvers = {
 
    Query: {
     welcome: () => `Hacker News clone begins.`,
+    links:   () => articleLinks,
    },
 }
 
