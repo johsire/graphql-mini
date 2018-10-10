e8591f8e0b810c736715e25cb1823531d12fad4f
Josiah Maina <josiah5115@gmail.com>
2018-10-10 10:58:54 -0600

    index.js init and dependencies

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// Graphql-yoga is like express for graph. It takes in the required setup and the 
+// Routes request to where they need to go.
+
+const { GraphQLServer } = require('graphql-yoga')
+
+const typeDefs = `
+
+type Query {
+ welcome: String!
+}
+`
+
+// This is the object that needs to match the stracture of our typeDefination Queries and Muatation
+// All values should be functions and what they return is lie doing res.send...and
+const resolvers = {
+
+   Query: {
+    welcome: () => `Hacker News clone begins.`,
+   },
+}
+
+// our server is looking for our typeDefs and our Resolvers
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:400`))
